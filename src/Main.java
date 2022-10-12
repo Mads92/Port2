caa826bcac3029c626f2389b7593c976395c6668
Mads92 <madskoie@hotmail.com>
2022-10-12 13:07:37 +0200

    Ports created, methods for shipping containers implemented,ass well as calculations of the number of excess/missing containers

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        Port jawah = new Port("Jawaharal Nehru", 2);
+        Port tanju = new Port("Tanjung Pelepas", 5);
+        Port dares = new Port("Dar Es Salaam", 3);
+        Port momba = new Port("Mombasa", 2);
+        Port zanzi = new Port("Zanzibar", 0);
+        Port jebel = new Port("Jebel Ali Dubai",0);
+        Port salah = new Port("Salalah", 0);
+
+        Port[] ports = new Port[] {jawah,tanju,dares,momba,zanzi,jebel,salah};
+        for (int i = 0; i < ports.length; i++) {
+            System.out.println(ports[i].name);
+        }
+        jawah.shipContainers(momba);
+        jawah.shipContainers(dares);
+
+        tanju.shipContainers(momba);
+        tanju.shipContainers(dares);
+        tanju.shipContainers(zanzi);
+        tanju.shipContainers(jebel);
+        tanju.shipContainers(salah);
+
+        dares.shipContainers(tanju);
+        dares.shipContainers(jawah);
+        dares.shipContainers(jebel);
+
+        momba.shipContainers(salah);
+        momba.shipContainers(jebel);
+
+        for (int i = 0; i < ports.length; i++) {
+            System.out.println(ports[i].containerDifference());
+        }
+        /*
+        System.out.println("Jawaharal Nehru:");
+        System.out.println(jawah.hasRequiredContainers());
+        System.out.println(jawah.containerDifference());
+        System.out.println("Momba:");
+        System.out.println(momba.hasRequiredContainers());
+        System.out.println(momba.containerDifference());
+
+         */
+    }
+}
---
+++ src/Port.java
@@ -0,0 +1,27 @@
+public class Port {
+    String name;
+    int containers;
+    int requiredContainers;
+
+    public Port(String name, int containers){
+        this.name = name;
+        this.containers = containers;
+        this.requiredContainers = containers;
+    }
+
+    public void shipContainers(Port p){
+        containers = containers - 1;
+        p.containers += 1;
+    }
+
+    public boolean hasRequiredContainers(){
+        return (requiredContainers == containers);
+    }
+
+    public int containerDifference(){
+        //If containerDifference is positive, the port has too many containers,
+        //if negative, the port needs containers,
+        //if 0, the port has the correct amount of containers
+        return  containers - requiredContainers;
+    }
+}
